bde96fd7c5a7239268e05e2e2cb0d06daed28ada
Bibek <bibek.roman1@gmail.com>
2023-04-21 09:43:38 +0530

    Front End with Match detail Card and Match Small Card

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { TeamPages } from './Pages/TeamPages';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <TeamPages/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/teams/:teamName" element={<TeamPages/>}></Route>
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
--- src/frontend/src/Components/MatchSmallCard.js
+++ src/frontend/src/Components/MatchSmallCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
-export const MatchSmallCard=({match})=> {
+import {Link} from 'react-router-dom'
+export const MatchSmallCard=({match,teamName})=> {
+  const otherTeam= match.team1==teamName ? match.team2 : match.team1;
+    const otherTeamRoute="/teams/${otherTeam}"
   return (
     <div className='matchSmallCard'>
-        <p>{match.team1} vs {match.team2}</p>
+        <p>vs <Link to={otherTeamRoute}>{otherTeam}</Link></p>
     </div>
   )
 }
